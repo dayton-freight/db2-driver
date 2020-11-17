2835d91b8979f79de0182149d67f40f77ef686cd
Ryan Cronin <rcronin@daytonfreight.com>
2020-11-17 13:52:05 -0500

    Update driver

--- project.clj
+++ project.clj
@@ -1,19 +1,13 @@
-(defproject metabase/crate-driver "1.0.1-crate-jdbc-2.3.0"
+(defproject metabase/db2-driver "1.0.0"
   :min-lein-version "2.5.0"
-
-  :dependencies
-  [[io.crate/crate-jdbc "2.3.0"
-    :exclusions [com.fasterxml.jackson.core/jackson-core
-                 org.slf4j/slf4j-api]]]
-
-  :repositories
-  [["bintray" "https://dl.bintray.com/crate/crate"]] ; Repo for Crate JDBC driver
 
   :profiles
   {:provided
-   {:dependencies
-    [[org.clojure/clojure "1.10.0"]
-     [metabase-core "1.0.0-SNAPSHOT"]]}
+   {:dependencies [
+     [org.clojure/clojure "1.10.0"]
+     [metabase-core "1.0.0-SNAPSHOT"]
+     [net.sf.jt400/jt400 "10.4"]
+    ]}
 
    :uberjar
    {:auto-clean    true
@@ -21,4 +15,4 @@
     :omit-source   true
     :javac-options ["-target" "1.8", "-source" "1.8"]
     :target-path   "target/%s"
-    :uberjar-name  "crate.metabase-driver.jar"}})
+    :uberjar-name  "db2.metabase-driver.jar"}})
